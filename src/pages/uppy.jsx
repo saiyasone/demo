81d520e5235e37c6155dd98c7fcf3fed122de3e3
lien teng <olienteng895@gmail.com>
2024-05-14 17:30:21 +0700

    uda

--- src/pages/uppy.jsx
+++ src/pages/uppy.jsx
@@ -1,9 +1,12 @@
 import React, { Fragment, useEffect, useState } from "react";
 import { Box, Typography } from "@mui/material";
+import CryptoJS from "crypto-js";
 import Uppy from "@uppy/core";
 import { Dashboard } from "@uppy/react";
 import GoogleDrive from "@uppy/google-drive";
 import Dropbox from "@uppy/dropbox";
+import Tus from "@uppy/tus";
+import xhrUpload from "@uppy/xhr-upload"
 import Instagram from "@uppy/instagram";
 import Url from "@uppy/url";
 import Webcam from "@uppy/webcam";
@@ -17,10 +20,25 @@ function UppyPackage() {
   const companionUrl = "https://companion.uppy.io";
   const [uppyInstance, setUppyInstance] = useState(() => new Uppy());
   useEffect(() => {
+    const secretKey = "jsje3j3,02.3j2jk=243j42lj34hj23l24l;2h5345l";
+    const headers = {
+      REGION: "sg",
+      BASE_HOSTNAME: "storage.bunnycdn.com",
+      STORAGE_ZONE_NAME: "beta-vshare",
+      ACCESS_KEY: "a4287d4c-7e6c-4643-a829f030bc10-98a9-42c3",
+      PATH: "6722542899692-114",
+      FILENAME: "114.png",
+      PATH_FOR_THUMBNAIL: "6722542899692-114",
+    };
+    const encryptedHeaders = CryptoJS.AES.encrypt(
+      JSON.stringify(headers),
+      secretKey
+    ).toString();
+
     const uppy = new Uppy({
       id: "upload-file-id",
       restrictions: {},
-      autoProceed: true,
+      autoProceed: false,
     })
       .use(Webcam)
       .use(GoogleDrive, {
@@ -37,15 +55,15 @@ function UppyPackage() {
         companionUrl,
       })
       .on("file-added", (files) => {
-        console.log("File added:", files.data);
+        // console.log("File added:", files.data);
       })
-      .on("upload", () => {
-        console.log("Uploading...");
+      .use(xhrUpload, {
+        endpoint: "https://load.vshare.net/upload",
+        headers: {
+          "Content-Type": "multipart/form-data",
+          encryptedHeaders,
+        },
       });
-    //   .use(Tus, {
-    //     endpoint: "https://load.vshare.net/upload",
-    //     "Content-Type": "multipart/form-data",
-    //   });
 
     setUppyInstance(uppy);
     return () => {
