aa58cde87b65bcb3b12363402e3013ae3c547d2b
saiyasone <saiyasone595@gmail.com>
2024-10-01 23:04:23 +0700

    changes

--- src/pages/manual-upload.jsx
+++ src/pages/manual-upload.jsx
@@ -1,6 +1,7 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { encryptData } from "../utils/secure";
 import Header from "../components/layouts/header";
+import { getFileNameExtension } from "../utils/file.util";
 const endpoints = "https://coding.load.vshare.net/";
 const userData = localStorage.getItem("userData")
   ? JSON.parse(localStorage.getItem("userData"))
@@ -9,10 +10,9 @@ const userData = localStorage.getItem("userData")
 async function initiateMultipartUpload(file) {
   const headers = {
     createdBy: userData?._id,
-    FILENAME: file.name,
+    FILENAME: file.newFilename,
     PATH: `${userData?.newName}-${userData?._id}}`,
   };
-
   const _encryptHeader = await encryptData(headers);
 
   const response = await fetch(`${endpoints}initiate-multipart-upload`, {
@@ -94,10 +94,19 @@ async function completeMultipartUpload(uploadId, parts, fileName) {
 function MultipleFileUpload() {
   const [selectedFiles, setSelectedFiles] = useState([]);
   const [uploadProgress, setUploadProgress] = useState([]);
+  const [totalProgress, setTotalProgress] = useState(0);
+  const [totalPartsUploaded, setTotalPartsUploaded] = useState(0);
+  const [uploadStatus, setUploadStatus] = useState([]); // Track status of each file
+  const [allUploaded, setAllUploaded] = useState(false);
+  const fileRef = useRef(null);
 
   const handleFileChange = (event) => {
     if (event.target.files) {
       const filesArray = Array.from(event.target.files);
+      setTotalProgress(0);
+      setTotalPartsUploaded(0);
+      setUploadStatus(false);
+      setUploadStatus(filesArray.map(() => "pending"));
       setSelectedFiles(filesArray);
       setUploadProgress(
         filesArray.map((file) => ({ file, progress: 0, status: "pending" }))
@@ -105,11 +114,11 @@ function MultipleFileUpload() {
     }
   };
 
-  const uploadFile = async (file, index) => {
+  const uploadFile = async (file, index, totalParts) => {
     try {
       const { uploadId } = await initiateMultipartUpload(file);
 
-      const partSize = 10 * 1024 * 1024; // 10 MB
+      const partSize = 5 * 1024 * 1024; // 5 MB
       const numberOfParts = Math.ceil(file.size / partSize);
       const parts = [];
 
@@ -121,7 +130,7 @@ function MultipleFileUpload() {
         const presignedUrl = await getPresignedUrl(
           uploadId,
           partNumber,
-          file.name
+          file.newFilename
         );
         const eTag = await uploadPart(presignedUrl, filePart);
 
@@ -132,11 +141,29 @@ function MultipleFileUpload() {
           (partNumber / numberOfParts) * 100
         );
         updateProgress(index, progressPercentage, "uploading");
+
+        // total progress
+        setTotalPartsUploaded((prevTotalParts) => {
+          const updatedTotalParts = prevTotalParts + 1;
+          const progressTotalPercentage = Math.round(
+            (updatedTotalParts / totalParts) * 100
+          );
+          console.log({ progressTotalPercentage });
+          setTotalProgress(
+            progressTotalPercentage > 100 ? 100 : progressTotalPercentage
+          );
+          return updatedTotalParts;
+        });
       }
 
       // 3. Complete multipart upload
-      await completeMultipartUpload(uploadId, parts, file.name);
+      await completeMultipartUpload(uploadId, parts, file.newFilename);
+
+      updateFileStatus(index, "completed");
+
       updateProgress(index, 100, "completed");
+
+      checkAllFilesUploaded();
     } catch (error) {
       updateProgress(index, 0, "failed");
     }
@@ -151,18 +178,80 @@ function MultipleFileUpload() {
   };
 
   const handleUpload = () => {
-    selectedFiles.forEach((file, index) => {
-      uploadFile(file, index);
+    const totalParts = selectedFiles.reduce((acc, file) => {
+      const partSize = 10 * 1024 * 1024; // 10 MB
+      return acc + Math.ceil(file.size / partSize);
+    }, 0);
+
+    selectedFiles.map((file, index) => {
+      const randomNewName = Math.floor(111111111 + Math.random() * 999999999);
+      const newFileName = `${randomNewName}${getFileNameExtension(file.name)}`;
+
+      const dataFile = file;
+      dataFile.newFilename = newFileName;
+
+      return uploadFile(dataFile, index, totalParts);
     });
   };
+
+  const checkAllFilesUploaded = () => {
+    if (uploadStatus.every((status) => status === "completed")) {
+      setAllUploaded(true);
+    }
+  };
+
+  const updateFileStatus = (index, status) => {
+    setUploadStatus((prevStatus) => {
+      const newStatus = [...prevStatus];
+      newStatus[index] = status;
+      return newStatus;
+    });
+  };
+
+  const handleClearData = () => {
+    fileRef.current.value = "";
+    setSelectedFiles([]);
+    setUploadProgress([]);
+    setTotalPartsUploaded(0);
+    setUploadStatus(false);
+  };
+
+  useEffect(() => {
+    if (uploadStatus.length > 0) {
+      if (uploadStatus.every((status) => status === "completed")) {
+        setAllUploaded(true);
+        handleClearData();
+      }
+    }
+  }, [uploadStatus]);
 
   return (
     <div>
       <Header />
       <div style={{ padding: "1rem" }}>
-        <input type="file" multiple onChange={handleFileChange} />
+        <input type="file" multiple onChange={handleFileChange} ref={fileRef} />
         <button onClick={handleUpload}>Upload Files</button>
 
+        <div>
+          <p>{JSON.stringify(uploadStatus)}</p>
+        </div>
+
+        <div style={{ margin: "2rem 0" }}>
+          <h2>Total progress {totalProgress}% </h2>
+          <progress value={totalProgress} max="100" />
+
+          {allUploaded && (
+            <h3
+              style={{
+                fontSize: "1.5rem",
+                color: "#17766B",
+                fontWeight: "bold",
+              }}
+            >
+              All files uploaded successfully!
+            </h3>
+          )}
+        </div>
         <div>
           {uploadProgress.map((progress, index) => (
             <div key={index}>
