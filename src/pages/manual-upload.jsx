d4800f89d4e0f23be159a37769daae9a086198a2
lien teng <olienteng895@gmail.com>
2024-10-02 11:00:39 +0700

    changes

--- src/pages/manual-upload.jsx
+++ src/pages/manual-upload.jsx
@@ -1,7 +1,8 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useMemo, useRef, useState } from "react";
 import { encryptData } from "../utils/secure";
 import Header from "../components/layouts/header";
-import { getFileNameExtension } from "../utils/file.util";
+import { convertBytetoMBandGB, getFileNameExtension } from "../utils/file.util";
+import { gql, useMutation } from "@apollo/client";
 const endpoints = "https://coding.load.vshare.net/";
 const userData = localStorage.getItem("userData")
   ? JSON.parse(localStorage.getItem("userData"))
@@ -11,7 +12,7 @@ async function initiateMultipartUpload(file) {
   const headers = {
     createdBy: userData?._id,
     FILENAME: file.newFilename,
-    PATH: `${userData?.newName}-${userData?._id}}`,
+    PATH: `${userData?.newName}-${userData?._id}/757688d3-4b0f-4dc5-ab73-daab881882a8`,
   };
   const _encryptHeader = await encryptData(headers);
 
@@ -30,7 +31,7 @@ async function getPresignedUrl(uploadId, partNumber, fileName) {
   const headers = {
     createdBy: userData?._id,
     FILENAME: fileName,
-    PATH: `${userData?.newName}-${userData?._id}}`,
+    PATH: `${userData?.newName}-${userData?._id}/757688d3-4b0f-4dc5-ab73-daab881882a8`,
   };
 
   const _encryptHeader = await encryptData(headers);
@@ -69,7 +70,7 @@ async function completeMultipartUpload(uploadId, parts, fileName) {
   const headers = {
     createdBy: userData?._id,
     FILENAME: fileName,
-    PATH: `${userData?.newName}-${userData?._id}}`,
+    PATH: `${userData?.newName}-${userData?._id}/757688d3-4b0f-4dc5-ab73-daab881882a8`,
   };
 
   const _encryptHeader = await encryptData(headers);
@@ -91,14 +92,29 @@ async function completeMultipartUpload(uploadId, parts, fileName) {
   return data;
 }
 
+const MUTATION_CREATE_FILE = gql`
+  mutation CreateFiles($data: FilesInput!) {
+    createFiles(data: $data) {
+      _id
+      path
+    }
+  }
+`;
+
 function MultipleFileUpload() {
   const [selectedFiles, setSelectedFiles] = useState([]);
   const [uploadProgress, setUploadProgress] = useState([]);
   const [totalProgress, setTotalProgress] = useState(0);
   const [totalPartsUploaded, setTotalPartsUploaded] = useState(0);
-  const [uploadStatus, setUploadStatus] = useState([]); // Track status of each file
+  const [uploadStatus, setUploadStatus] = useState([]);
+  const [totalTimeTaken, setTotalTimeTaken] = useState(null);
+  const [elapsedTime, setElapsedTime] = useState(0); // Track real-time elapsed time
+  const [timer, setTimer] = useState(null);
+  const [uploadTimes, setUploadTimes] = useState([]);
   const [allUploaded, setAllUploaded] = useState(false);
   const fileRef = useRef(null);
+
+  const [uploadFiles] = useMutation(MUTATION_CREATE_FILE);
 
   const handleFileChange = (event) => {
     if (event.target.files) {
@@ -115,6 +131,8 @@ function MultipleFileUpload() {
   };
 
   const uploadFile = async (file, index, totalParts) => {
+    const startTime = performance.now();
+
     try {
       const { uploadId } = await initiateMultipartUpload(file);
 
@@ -148,10 +166,11 @@ function MultipleFileUpload() {
           const progressTotalPercentage = Math.round(
             (updatedTotalParts / totalParts) * 100
           );
-          console.log({ progressTotalPercentage });
+
           setTotalProgress(
-            progressTotalPercentage > 100 ? 100 : progressTotalPercentage
+            progressTotalPercentage >= 100 ? 99 : progressTotalPercentage
           );
+
           return updatedTotalParts;
         });
       }
@@ -162,6 +181,15 @@ function MultipleFileUpload() {
       updateFileStatus(index, "completed");
 
       updateProgress(index, 100, "completed");
+
+      const endTime = performance.now(); // End tracking time
+      const timeTaken = (endTime - startTime) / 1000; // Time in seconds
+
+      setUploadTimes((prevTimes) => {
+        const newTimes = [...prevTimes];
+        newTimes[index] = timeTaken;
+        return newTimes;
+      });
 
       checkAllFilesUploaded();
     } catch (error) {
@@ -177,21 +205,59 @@ function MultipleFileUpload() {
     });
   };
 
-  const handleUpload = () => {
-    const totalParts = selectedFiles.reduce((acc, file) => {
-      const partSize = 10 * 1024 * 1024; // 10 MB
-      return acc + Math.ceil(file.size / partSize);
-    }, 0);
+  const handleUpload = async () => {
+    if (selectedFiles.length > 0) {
+      const totalParts = selectedFiles.reduce((acc, file) => {
+        const partSize = 5 * 1024 * 1024; // 5 MB
+        return acc + Math.ceil(file.size / partSize);
+      }, 0);
+      const startTime = performance.now();
+      setTimer(
+        setInterval(() => {
+          setElapsedTime((performance.now() - startTime) / 1000); // Update elapsed time in seconds
+        }, 1000)
+      );
 
-    selectedFiles.map((file, index) => {
-      const randomNewName = Math.floor(111111111 + Math.random() * 999999999);
-      const newFileName = `${randomNewName}${getFileNameExtension(file.name)}`;
+      const uploadPromise = selectedFiles.map(async (file, index) => {
+        const randomNewName = Math.floor(111111111 + Math.random() * 999999999);
+        const newFileName = `${randomNewName}${getFileNameExtension(
+          file.name
+        )}`;
 
-      const dataFile = file;
-      dataFile.newFilename = newFileName;
+        const uploading = await uploadFiles({
+          variables: {
+            data: {
+              destination: "",
+              newFilename: newFileName,
+              filename: file.name,
+              fileType: file.type,
+              size: file.size.toString(),
+              checkFile: "sub",
+              country: "india",
+              device: "pc",
+              totalUploadFile: selectedFiles.length,
+              newPath: `${"757688d3-4b0f-4dc5-ab73-daab881882a8"}/${newFileName}`,
+              folder_id: "728",
+            },
+          },
+        });
 
-      return uploadFile(dataFile, index, totalParts);
-    });
+        const fileId = await uploading.data?.createFiles?._id;
+
+        if (fileId) {
+          const dataFile = file;
+          dataFile.newFilename = newFileName;
+
+          return uploadFile(dataFile, index, totalParts);
+        }
+      });
+
+      await Promise.all(uploadPromise);
+      setTotalProgress(100);
+      const endTime = performance.now();
+      const totalTime = (endTime - startTime) / 1000; // Time in seconds
+      setTotalTimeTaken(totalTime);
+    }
   };
 
   const checkAllFilesUploaded = () => {
@@ -210,11 +276,33 @@ function MultipleFileUpload() {
 
   const handleClearData = () => {
     fileRef.current.value = "";
-    setSelectedFiles([]);
-    setUploadProgress([]);
     setTotalPartsUploaded(0);
     setUploadStatus(false);
+    clearInterval(timer);
   };
+
+  const resetFileData = () => {
+    fileRef.current.value = "";
+    setUploadStatus(false);
+    setTotalPartsUploaded(0);
+    setTotalProgress(0);
+    setAllUploaded(false);
+    setTimer(null);
+    clearInterval(timer);
+    setElapsedTime(0);
+    setTotalTimeTaken(null);
+
+    setUploadTimes([]);
+    setSelectedFiles([]);
+    setUploadProgress([]);
+  };
+
+  const timeTakenData = useMemo(() => {
+    const minutes = Math.floor(elapsedTime / 60);
+    const seconds = elapsedTime % 60;
+
+    return { minutes, seconds };
+  }, [elapsedTime]);
 
   useEffect(() => {
     if (uploadStatus.length > 0) {
@@ -230,15 +318,39 @@ function MultipleFileUpload() {
       <Header />
       <div style={{ padding: "1rem" }}>
         <input type="file" multiple onChange={handleFileChange} ref={fileRef} />
-        <button onClick={handleUpload}>Upload Files</button>
-
-        <div>
-          <p>{JSON.stringify(uploadStatus)}</p>
+        <div style={{ margin: "0.8rem 0", display: "flex", gap: "12px" }}>
+          <button onClick={handleUpload}>Upload Files</button>
+          <button onClick={resetFileData}>Reset Files</button>
         </div>
 
         <div style={{ margin: "2rem 0" }}>
           <h2>Total progress {totalProgress}% </h2>
           <progress value={totalProgress} max="100" />
+
+          {totalTimeTaken && (
+            <h3
+              style={{
+                fontSize: "1.5rem",
+                color: "#17766B",
+                fontWeight: "bold",
+              }}
+            >
+              Time taken: {totalTimeTaken.toFixed(2)} seconds
+            </h3>
+          )}
+
+          <h4> elapsed: {elapsedTime.toFixed(0)} </h4>
+
+          <h4>
+            {" "}
+            times:{" "}
+            {timeTakenData.minutes === 0
+              ? ""
+              : timeTakenData.minutes.toFixed(0) + " m"}{" "}
+            {timeTakenData.seconds === 0
+              ? ""
+              : timeTakenData.seconds.toFixed(0) + " s"}
+          </h4>
 
           {allUploaded && (
             <h3
@@ -254,12 +366,27 @@ function MultipleFileUpload() {
         </div>
         <div>
           {uploadProgress.map((progress, index) => (
-            <div key={index}>
-              <p>{progress.file.name}</p>
+            <div
+              key={index}
+              style={{
+                border: "1px solid #ccc",
+                padding: "1rem",
+                margin: "0.7rem 0",
+                borderRadius: "5px",
+              }}
+            >
+              <p>
+                {progress.file.name} {progress.progress}%{" "}
+                {convertBytetoMBandGB(progress.file?.size || 0)}{" "}
+              </p>
               <p>Status: {progress.status}</p>
               <progress value={progress.progress} max="100">
                 {progress.progress}%
               </progress>
+
+              {uploadTimes[index] && (
+                <p>Time taken: {uploadTimes[index].toFixed(2)} seconds</p>
+              )}
             </div>
           ))}
         </div>
